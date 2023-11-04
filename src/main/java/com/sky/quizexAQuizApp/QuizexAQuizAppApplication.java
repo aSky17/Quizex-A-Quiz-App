be9e6c00a766b9825e7efbbe5e352e35444582d7
aSky17 <akashmishra.am.1710@gmail.com>
2023-11-04 09:04:30 +0530

    Initialized spring app

---
+++ src/main/java/com/sky/quizexAQuizApp/QuizexAQuizAppApplication.java
@@ -0,0 +1,13 @@
+package com.sky.quizexAQuizApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizexAQuizAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizexAQuizAppApplication.class, args);
+	}
+
+}
